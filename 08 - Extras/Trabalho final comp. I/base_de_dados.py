e232bcbda7debd0799dbd859c6dfe38f66ffa071
Pedro Paulo <epedropaulodsm@gmail.com>
2022-03-22 18:30:04 -0300

    Adicionando a função lambda, junto a outras ferramentas como o filter e o map.

--- 08 - Extras/Trabalho final comp. I/base_de_dados.py
+++
@@ -1,5 +0,0 @@
-base_de_dados = {
-    'Peixes': ['baiacu', 'atum', 'bagre', 'salmao', 'tubarao', 'piranha', 'girino', 'lula', 'polvo', 'siri'],
-    'Frutas': ['banana', 'tangerina', 'morango', 'pera', 'laranja', 'uva', 'kiwi', 'abacaxi', 'abacate', 'tomate'],
-    'Cozinha': ['faca', 'frigideira', 'panela', 'sal', 'garfo', 'copo', 'tampa', 'geladeira', 'detergente', 'prato'],
-    'Banheiro': ['vaso', 'toalha', 'pente', 'shampoo', 'escova', 'espelho', 'chuveiro', 'ralo', 'torneira', 'sabonete']}
