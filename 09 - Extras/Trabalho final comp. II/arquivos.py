afdf15591c35cad770d917618e73f95740125b52
Pedro Paulo <epedropaulodsm@gmail.com>
2022-07-19 23:00:50 -0300

    Add.: Conterá as classes de tratamento de arquivos.

---
+++ 09 - Extras/Trabalho final comp. II/arquivos.py
@@ -0,0 +1,25 @@
+# Ler os arquivos usando pandas
+# O arquivo será csv e as colunas serão:
+# UM ARRAY COM AS PILHAS - QUANTIDADE DE BLOCOS USADOS - PONTOS
+
+class Arquivos:
+    """Classe que cuidará dos arquivos, saves e estatísticas."""
+
+    def salvar(pilhas, nBlocos, pontos):
+        """Armazena os dados em um arquivo csv, recebe as pilhas em um array, a quantidade de blocos usados 
+        e os pontos.
+        Array, int, int -> csv"""
+        pass
+
+    def leitura_carregar_save(arquivo):
+        """Dado um arquivo csv de save do jogo, retorna informações essenciais para o carregamento
+        do jogo. Retornará o array com as pilhas
+        csv -> Array"""
+        pass
+
+    def leitura_estatistica():
+        """Dado um arquivo csv de save do jogo, retorna informações essenciais para a formação das
+        estatísticas. Retornará a quantidade de blocos usados e os pontos.
+        csv -> int, int"""
+        pass
+    
