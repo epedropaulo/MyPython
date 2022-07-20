6b3c82214393170958a8f72339cce5611c2bdfd8
Pedro Paulo <epedropaulodsm@gmail.com>
2022-07-19 23:02:12 -0300

    Add.: Conterá toda a análise estatística do jogo.

---
+++ 09 - Extras/Trabalho final comp. II/estatistica.py
@@ -0,0 +1,9 @@
+# Matplotlib no tkinter.
+
+
+class Estatistica:
+    """Dado um arquivo de estatísticas as analisará e as exibirá no tkinter"""
+
+    def plotar(nBlocos, pontos):
+        """Recebe o número de blocos e os pontos e cria um gráfico, onde nblocos e o eixo x e os pontos o y."""
+        pass
