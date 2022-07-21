2c231120fccc21c0db29ef790b650fe1048acc3d
Pedro Paulo <epedropaulodsm@gmail.com>
2022-07-21 00:09:49 -0300

    modified: Antes considerei o jogo errado, agora ta certo.

--- 09 - Extras/Trabalho final comp. II/arquivos.py
+++ 09 - Extras/Trabalho final comp. II/arquivos.py
@@ -1,25 +1,16 @@
 # Ler os arquivos usando pandas
 # O arquivo será csv e as colunas serão:
-# UM ARRAY COM AS PILHAS - QUANTIDADE DE BLOCOS USADOS - PONTOS
+# ARRAY COM A MATRIZ DA TELA - NÚMERO PRA ESQUERDA - NÚMERO PRA DIREITA - NÚMERO PRA CIMA - NÚMERO PRA BAIXO
 
 class Arquivos:
     """Classe que cuidará dos arquivos, saves e estatísticas."""
 
-    def salvar(pilhas, nBlocos, pontos):
-        """Armazena os dados em um arquivo csv, recebe as pilhas em um array, a quantidade de blocos usados 
-        e os pontos.
-        Array, int, int -> csv"""
+    def salvar(matriz, dados):
+        """Recebe a matriz que é representada na tela e uma lista com os dados. E armazena em um csv.
+        Array, list-> csv"""
         pass
 
     def leitura_carregar_save(arquivo):
-        """Dado um arquivo csv de save do jogo, retorna informações essenciais para o carregamento
-        do jogo. Retornará o array com as pilhas
-        csv -> Array"""
+        """Dado um arquivo csv de save do jogo, retorna as informações para carregar o jogo.
+        csv -> Array, list"""
         pass
-
-    def leitura_estatistica():
-        """Dado um arquivo csv de save do jogo, retorna informações essenciais para a formação das
-        estatísticas. Retornará a quantidade de blocos usados e os pontos.
-        csv -> int, int"""
-        pass
-    
